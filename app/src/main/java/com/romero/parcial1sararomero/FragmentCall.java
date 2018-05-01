bbb6719cc8854b8775ccb8310c5fae8e74bed738
Sara <00030716@uca.edu.sv>
2018-05-01 15:13:20 -0600

    Initial commit

---
+++ app/src/main/java/com/romero/parcial1sararomero/FragmentCall.java
@@ -0,0 +1,21 @@
+package com.romero.parcial1sararomero;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Sara on 1/05/2018.
+ */
+public class FragmentCall extends Fragment {
+
+    public FragmentCall() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        return super.onCreateView(inflater,container,savedInstanceState);
+    }
+}
